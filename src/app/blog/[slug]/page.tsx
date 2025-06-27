13bf6fd9488215d9333f159e68f57554bf12ecee
Adam Danielsson <adam.ov.danielsson@gmail.com>
2025-06-27 12:38:05 +0200

    feat: project structure

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,8 +1,10 @@
-import { PortableText, type SanityDocument } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-import { client } from "@/sanity/client";
-import Link from "next/link";
+import Link from 'next/link';
+
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { PortableText, type SanityDocument } from 'next-sanity';
+
+import { client } from '@/lib/sanity';
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
@@ -29,7 +31,7 @@ export default async function PostPage({
     : null;
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
+    <main className="container mx-auto flex min-h-screen max-w-3xl flex-col gap-4 p-8">
       <Link href="/" className="hover:underline">
         ← Back to posts
       </Link>
@@ -42,7 +44,7 @@ export default async function PostPage({
           height="310"
         />
       )}
-      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+      <h1 className="mb-8 text-4xl font-bold">{post.title}</h1>
       <div className="prose">
         <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
         {Array.isArray(post.body) && <PortableText value={post.body} />}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import { type SanityDocument } from "next-sanity";
+import Link from 'next/link';
 
-import { client } from "@/sanity/client";
+import { type SanityDocument } from 'next-sanity';
+
+import { client } from '@/lib/sanity';
 
 const POSTS_QUERY = `*[
   _type == "post"
@@ -15,7 +16,7 @@ export default async function IndexPage() {
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Posts</h1>
+      <h1 className="mb-8 text-4xl font-bold">Posts</h1>
       <ul className="flex flex-col gap-y-4">
         {posts.map((post) => (
           <li className="hover:underline" key={post._id}>
