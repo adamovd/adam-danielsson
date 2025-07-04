2c18aaf7cda77c7808cd8f67ffb2b9d35bba23bd
Adam Danielsson <adam.ov.danielsson@gmail.com>
2025-07-04 16:03:24 +0200

    styles for hero component

--- src/components/ui/hero/hero.tsx
+++ src/components/ui/hero/hero.tsx
@@ -1,11 +1,24 @@
 import { PortableText } from 'next-sanity';
 
+import urlFor from '@/lib/sanity-image';
 import { Hero } from '@/types/sanity.types';
 
 const HeroComponent = ({ title, body, image }: Hero) => {
+  const heroImageUrl = image
+    ? urlFor(image)?.width(550).height(310).url()
+    : undefined;
   return (
-    <section>
-      <h1>{title}</h1>
+    <section className="flex h-screen w-full flex-col items-center justify-center gap-6 bg-secondary p-6">
+      {heroImageUrl && (
+        <div className="mb-6">
+          <img
+            src={heroImageUrl}
+            alt={'Hero Image'}
+            className="h-auto w-full rounded-lg"
+          />
+        </div>
+      )}
+      <h1 className="font-serif text-4xl">{title}</h1>
       <PortableText value={body || []} />
     </section>
   );
